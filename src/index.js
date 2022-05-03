89b3a6acddae93878a7b7caf34fdda3513ce927e
dmorita00 <ingressmrt@gmail.com>
2022-05-03 23:43:45 +0900

    first

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const init = () => {
+    const table = document.getElementById('fixed-table')
+    if (!table) return
+
+    table.innerHTML(
+`
+<table>
+  <thead>
+    <tr>
+      <th>A</th>
+      <th>B</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>A1</td>
+      <td>B1</td>
+    </tr>
+    <tr>
+      <td>A2</td>
+      <td>B2</td>
+    </tr>
+  </tbody>
+</table>
+`
+    )
+}
+
+init()
