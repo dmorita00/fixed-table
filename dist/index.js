9a886c6e3ff0e635a6cb1e972a9e6b536eb2599a
dmorita00 <ingressmrt@gmail.com>
2022-05-04 00:33:43 +0900

    first

---
+++ dist/index.js
@@ -0,0 +1,33 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.ts":
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+/***/ (() => {
+
+eval("\nvar fixedTable = function () {\n    var table = document.getElementById('fixed-table');\n    if (!table)\n        return;\n    table.innerHTML = \"\\n<table>\\n  <thead>\\n    <tr>\\n      <th>A</th>\\n      <th>B</th>\\n    </tr>\\n  </thead>\\n  <tbody>\\n    <tr>\\n      <td>A1</td>\\n      <td>B1</td>\\n    </tr>\\n    <tr>\\n      <td>A2</td>\\n      <td>B2</td>\\n    </tr>\\n  </tbody>\\n</table>\\n\";\n};\n\n\n//# sourceURL=webpack://fixed-table/./src/index.ts?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.ts"]();
+/******/ 	
+/******/ })()
+;
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,23 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
-  mode: "development",
-  entry: './test/index.html',
-  devServer: {
+  mode: 'development',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: [
+      '.ts', '.js',
+    ],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.join(__dirname, 'dist')
   }
-}
+};
